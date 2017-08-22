f71dd9630141ef1193b5ad1c4c2ccc39ce64e740
V <reducedprocess@gmail.com>
2017-08-21 21:30:39 -0400

    logs update

--- logs/log.bat
+++ logs/log.bat
@@ -2,20 +2,24 @@
 
 call credentials.bat
 
-set /p input="Log query: "
+:START
+set /p input="log_q: "
 
 if "%input%" == "parse" (
 	ruby log_parse.rb
+	goto :START
 )
 
 if "%input%" == "repo" (
 	git add --all
 	git commit -m "logs update"
 	git push -u origin master
+	goto :START
 )
 
 if "%input%" == "database" (
 	mysql -h "%host%" -u "%user%" --password="%password%"
+	goto :START
 )
 
 cmd /k
